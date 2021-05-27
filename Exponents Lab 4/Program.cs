4a2dc5f9562d914381ac38b4f0fcd2cd90c9f8d0
jerome <jeromebrown97@gmail.com>
2021-05-27 06:16:42 -0500

    first commit for lab 4 EXPONENTS / Powers Table

---
+++ Exponents Lab 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exponents_Lab_4
+{
+    class Program
+    {
+
+     
+  
+        static void Main(string[] args)
+        {
+
+
+            int userInput;
+            string userContinue = "y";
+
+            while (userContinue == "y")
+            {
+            Console.WriteLine("Enter an integer");
+            userInput = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+                if (userInput > 1 && userInput < 10)
+                {
+                Console.WriteLine("Number\tSquare\tCube");
+                    for (int x = 0; x <= userInput; x = x + 1)
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}", x, x * x, x * x * x);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid number try again");
+                }
+                Console.WriteLine("Would you like to continue? y/n");
+                userContinue = Console.ReadLine();
+            }
+        }
+    }
+}
+
+
